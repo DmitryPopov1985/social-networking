66699f25d963c479e49d0bf37c2e80e15afcc6e5
Дмитрий Попов <mitjajwars@mail.ru>
2023-10-08 10:27:51 +0300

    first init

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+export function Navbar() {
+  return (
+    <nav className="nav">
+    <div>
+      <a href="#p">Profile</a>
+    </div>
+    <div>
+      <a href="#m">Messages</a>
+    </div>
+    <div>
+      <a href="#n">News</a>
+    </div>
+    <div>
+      <a href="#m">Music</a>
+    </div>
+    <div>
+      <a href="#s">Settings</a>
+    </div>
+  </nav>
+  )
+}
---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,16 @@
+
+import React from 'react'
+
+export function Profile() {
+  return (
+    <div><div className="content">
+    <div>Большая картинка</div>
+    <div>аватар + описание</div>
+    <div>Мои посты</div>
+    <div>Новые посты</div>
+    <div>пост1</div>
+    <div>пост2</div>
+  </div></div>
+  )
+}
+
