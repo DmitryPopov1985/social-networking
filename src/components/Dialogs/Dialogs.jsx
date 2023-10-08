46cc4a54dc6e18986722cac95415d0e97730f3c0
Дмитрий Попов <mitjajwars@mail.ru>
2023-10-08 17:23:33 +0300

    fixed Dialogs and MyPosts

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -2,20 +2,28 @@ import React from 'react'
 import cl from './Dialogs.module.css'
 import { NavLink } from 'react-router-dom'
 
+const dialogsData = [
+  {id: '1', name: "Дмитрий"},
+  {id: '2', name: "Петр"},
+  {id: '3', name: "Василий"},
+  
+]
+const messagesData = [
+  {id: '1', message: "Hi"},
+  {id: '2', message: "Yo"},
+  {id: '3', message: "Yo"},
+  
+]
 export function Dialogs() {
   
   return (
     <div className={cl.dialogsWrapper} >
       <div className={cl.dialogs} >
-        <DialogItem name='Дмитрий' id='1' />
-        <DialogItem name='Петр' id='2' />
-        <DialogItem name='Василий' id='3' />
+        {dialogsData.map(el => <DialogItem name={el.name} id={el.id} />)}
         
       </div>
       <div className={cl.messages} >
-        <Message message='hi' />
-        <Message message='yo' />
-        <Message message='yo' />
+        {messagesData.map(el => <Message message={el.message} />)}
       </div>
     </div>
   )
--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -2,15 +2,21 @@ import React from 'react'
 import cl from './MyPosts.module.css'
 import { Post } from './Post/Post'
 
+const postsData = [
+  { id: '1', message: "Хорошая погода", likesCount: 7 },
+  { id: '2', message: "Как дела?", likesCount: 0 },
+  { id: '3', message: "Василий, привет", likesCount: 18 },
+
+]
+
 export function MyPosts() {
   return (
     <div>Мои посты
-        <div>
-            <textarea ></textarea>
-            <button>Add post</button>
-        </div>
-        <Post message = 'hi'/>
-        <Post message = 'whe are you?'/>
+      <div>
+        <textarea ></textarea>
+        <button>Add post</button>
+      </div>
+      {postsData.map(el => <Post message={el.message} likesCount={el.likesCount} />)}
     </div>
   )
 }
